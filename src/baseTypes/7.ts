5073ea51495edeee4f87c52d309f41102887afe5
MaksymChukhrai <106784292+MaksymChukhrai@users.noreply.github.com>
2023-10-11 19:00:12 +0300

    1

--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -3,3 +3,25 @@
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
 
+enum Days {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday,
+}
+
+function isWeekend(day: Days): boolean {
+  if (day === Days.Saturday || day === Days.Sunday) {
+    return true; // Это выходные дни
+  } else {
+    return false; // Это дни недели
+  }
+}
+
+// Пример использования
+const today = Days.Saturday;
+const isTodayWeekend = isWeekend(today);
+console.log(`Сегодня выходной? ${isTodayWeekend}`);
